05a8a38e0ffaf8f589b839fb3b569330c7295196
Rod McLaughlin <rodmclaughlin@gmail.com>
2014-02-01 16:02:47 -0800

    Added Rails app to ischallenges/data-engineering

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DataEngineering::Application.config.secret_key_base = '93783fbe414124c9e03d9048318d54b26c0591dd43ca7217c21350c33e6a24fa5fba9e6aa1646de7f3c3222781f4b16bcde7b56e7aee5cbc1d4ee3a78c91150f'
