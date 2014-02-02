b7023d225cbbe997799b3ac3421e15de87cfaa9c
Rod McLaughlin <rodmclaughlin@gmail.com>
2014-02-02 10:55:28 -0800

    A new migration and a model to store the contents of the imported tsv file, and a failing test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe User do
 
-  let(:filepath) { File.join( Rails.root, "example_input.tab" ) }
+  let(:file_path) { File.join( Rails.root, "example_input.tab" ) }
   let(:user_email) { 'user@data-engineering.com' }
   let(:user_password) { 'foobar' }
   @user = nil
@@ -12,15 +12,23 @@ describe User do
     if @user.nil?
       @user = User.new(email: user_email, :password => user_password, :password_confirmation => user_password)
     end
-    @user.uploader = File.open(filepath)
-    @user.save!
   end
 
   it "should upload a file" do
+    @user.uploader = File.open(file_path)
+    @user.save!
     url = @user.uploader.url
     expect( url ).to match(/\/uploads\/user\/uploader\/[0-9]+\/example_input.tab/)
     path = File.join( Rails.root, 'public', url )
     expect( @user.uploader.current_path ).to eq( path )
     expect( @user.uploader.identifier ).to eq( 'example_input.tab' )
+  end
+
+  it "should save the contents of the file in the database" do
+    @user.uploader = File.open(file_path)
+    @user.save!
+    @user.calculate!
+
+
   end
 end
