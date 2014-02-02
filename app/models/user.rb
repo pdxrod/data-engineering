b389856b39104cef1faba09a985aa419564f0599
Rod McLaughlin <rodmclaughlin@gmail.com>
2014-02-02 10:12:19 -0800

    Needed to add this migration rails g migration AddUploaderToUsers uploader:string rake db:migrate RAILS_ENV=test to make user_spec pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
+require 'carrierwave/orm/activerecord'
+
 class User < ActiveRecord::Base
+
+  acts_as_authentic do # Authlogic
+  end
+
   mount_uploader :uploader, FileUploader
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:filepath) { File.join( Rails.root, "example_input.tab" ) }
+
+  it "should upload a file" do
+    u = User.new(email: 'user@data-engineering.com', :password => 'foobar', :password_confirmation => 'foobar')
+    u.uploader = File.open(filepath)
+    u.save!
+    u.uploader.url # => '/url/to/file.png'
+    u.uploader.current_path # => 'path/to/file.png'
+    u.uploader.identifier # => 'file.png'
+
+  end
 end
